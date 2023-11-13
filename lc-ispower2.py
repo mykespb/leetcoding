517ae204e2add89cf1160efbeadb9871b9dce3a4
mykespb <mykespb@gmail.com>
2023-11-13 20:08:19 +0300

    renew power of 2

--- lc-ispower2.py
+++ lc-ispower2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # ls-ispower2.py
 # (C) Mikhail Kolodin, 2023
-# 2023-01-27 2023-01-27 1.1
+# 2023-01-27 2023-11-13 2.0
 # LeetCode problem
 
 # Степень двойки (https://leetcode.com/problems/power-of-two)
@@ -11,7 +11,8 @@
 # Ввод: n = 1
 # Вывод: true
 
-def check(n: int) -> bool:
+def check1(n: int) -> bool:
+    """test for power of 2"""
     if n <= 0:
         return False
     if n < 3:
@@ -20,7 +21,26 @@ def check(n: int) -> bool:
         return False
     return check(n//2)
 
+
+def check2(n: int) -> bool:
+    """test for power of 2"""
+
+    if n < 1:
+        return False
+    if n < 3:
+        return True
+
+    t = 4
+    while t <= n:
+        if t == n:
+            return True
+        t *= 2
+
+    return False
+
 # tests
+
+check = check2
 
 print(1, check(1))
 print(2, check(2))
